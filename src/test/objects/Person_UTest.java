f1cbb4aedadb317fe3db79d85f857e74892a3cf4
n00bhax <28760191+n00bhax@users.noreply.github.com>
2020-12-19 09:47:11 +0100

    shitty mockito

---
+++ src/test/objects/Person_UTest.java
@@ -0,0 +1,27 @@
+package objects;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Person_UTest {
+
+    public Person_UTest(){
+
+    }
+
+    @Before
+    public void initialize()
+    {
+
+    }
+
+    @Test
+    public void t_getName() throws Exception{
+/*
+        Person mock_p = Mockito.mock(Person.class);
+        Mockito.when(mock_p.getName()).thenReturn(Person.class.getDeclaredField("name"));
+        field.setAccessible(true);
+        */
+
+    }
+}
